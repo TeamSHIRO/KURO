5b9cb8d389f5defa8a4a1490635cbb643630ed9d
TheMonHub <mono15.parnthakote@gmail.com>
2025-12-03 21:05:34 +0700

    Exclude `CMakeFiles` directory from `clang-format` processing in automation scripts.
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>

--- automated/clang-format-fix.sh
+++ automated/clang-format-fix.sh
@@ -2,4 +2,4 @@
 
 cd "$(dirname "$0")"/.. || error_exit "Failed to change directory"
 shopt -s globstar
-find . -type f \( -name "*.h" -o -name "*.c" \) -not -path "*/_deps/*" -exec clang-format -i -style=file --verbose {} +
+find . -type f \( -name "*.h" -o -name "*.c" \) -not -path "*/_deps/*" -not -path "*/CMakeFiles/*" -exec clang-format -i -style=file --verbose {} +
