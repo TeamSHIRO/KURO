8d7c8a56cd3d121e49db302e3bcaf380e0f06475
TheMonHub <mono15.parnthakote@gmail.com>
2025-12-13 18:26:49 +0700

    Add initial UEFI bootloader implementation
    - Introduce `CMakeLists.txt` for bootloader and toolchain setup.
    - Implement file handling and volume access in `file.c` and `file.h`.
    - Define bootloader entry point in `main.c` and `main.h`.
    - Add license header template in `docs/license-header-template.md`.
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>

---
+++ bootloader/src/file/file.c
@@ -0,0 +1,42 @@
+/*
+ * File: file.c
+ * Description: File-related functions.
+ *
+ * Copyright (C) 2025 TheMonHub
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "file/file.h"
+
+#include <efi.h>
+#include <protocol/efi-lip.h>
+#include <protocol/efi-sfsp.h>
+#include <stddef.h>
+
+#include "main.h"
+
+EFI_FILE_PROTOCOL *get_volume_handle(EFI_HANDLE img_handle)
+{
+    EFI_LOADED_IMAGE_PROTOCOL *loaded_image = NULL;
+    EFI_GUID lip_guid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
+    EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *sfsp;
+    EFI_GUID sfsp_guid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
+    EFI_FILE_PROTOCOL *volume_handle;
+
+    g_system_table->BootServices->OpenProtocol(img_handle, &lip_guid, (void **)&loaded_image, g_image_handle, NULL,
+                                               EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+    g_system_table->BootServices->OpenProtocol(loaded_image->DeviceHandle, &sfsp_guid, (void **)&sfsp, g_image_handle,
+                                               NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+    sfsp->OpenVolume(sfsp, &volume_handle);
+
+    return volume_handle;
+}
+
+UINT64 get_file_size(EFI_FILE_PROTOCOL *file)
+{
+    UINT64 size;
+    file->SetPosition(file, 0xFFFFFFFFFFFFFFFF);
+    file->GetPosition(file, &size);
+    file->SetPosition(file, 0);
+    return size;
+}
