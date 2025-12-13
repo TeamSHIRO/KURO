46e7be187a379c49dac972c2fc10ef00162ea03e
TheMonHub <mono15.parnthakote@gmail.com>
2025-12-13 18:26:49 +0700

    Add initial UEFI bootloader implementation
    - Introduce `CMakeLists.txt` for bootloader and toolchain setup.
    - Implement file handling and volume access in `file.c` and `file.h`.
    - Define bootloader entry point in `main.c` and `main.h`.
    - Add license header template in `docs/license-header-template.md`.
    
    Signed-off-by: TheMonHub <mono15.parnthakote@gmail.com>

---
+++ bootloader/src/main.c
@@ -0,0 +1,57 @@
+/*
+ * File: main.c
+ * Description: Entry point.
+ *
+ * Copyright (C) 2025 TheMonHub
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "main.h"
+
+#include <efi.h>
+
+#include "file/file.h"
+
+EFI_HANDLE g_image_handle;
+EFI_SYSTEM_TABLE *g_system_table;
+
+CHAR16 *hello_str = L"Hello, World!\r\n";
+
+EFI_STATUS efi_main(EFI_HANDLE image_handle_p, EFI_SYSTEM_TABLE *system_table_p)
+{
+    g_image_handle = image_handle_p;
+    g_system_table = system_table_p;
+
+    g_system_table->ConOut->OutputString(g_system_table->ConOut, L"Hello, World!\r\n");
+
+    EFI_FILE_PROTOCOL *volume = get_volume_handle(g_system_table);
+
+    EFI_FILE_PROTOCOL *file_protocol;
+    volume->Open(volume, &file_protocol, L"something", EFI_FILE_MODE_READ, 0);
+
+    UINTN file_size = (UINTN)get_file_size(file_protocol);
+
+    void *input_buffer;
+    g_system_table->BootServices->AllocatePool(EfiLoaderData, file_size, &input_buffer);
+
+    file_protocol->Read(file_protocol, &file_size, input_buffer);
+
+    const UINTN char_count = file_size / sizeof(CHAR16);
+
+    CHAR16 *out;
+    g_system_table->BootServices->AllocatePool(EfiLoaderData, (char_count + 3) * sizeof(CHAR16), (void **)&out);
+
+    const CHAR16 *in = (CHAR16 *)input_buffer;
+    for (UINTN i = 0; i < char_count; ++i)
+    {
+        const CHAR16 c = in[i];
+        out[i] = (c >> 8) | (c << 8);
+    }
+
+    out[char_count] = L'\r';
+    out[char_count + 1] = L'\n';
+    out[char_count + 2] = L'\0';
+
+    g_system_table->ConOut->OutputString(g_system_table->ConOut, out);
+    return (EFI_SUCCESS);
+}
---
+++ bootloader/toolchain/efi.cmake
@@ -0,0 +1,41 @@
+# Build options:
+# efi-clang - path to EFI-clang repository. if empty, it will be fetched automatically. Useful for offline builds.
+#
+
+set(efi-clang "" CACHE PATH "Path to EFI-clang repository. if empty, it will be fetched automatically.")
+
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_C_COMPILER_ID_RUN TRUE)
+set(CMAKE_C_COMPILER_ID Clang)
+set(CMAKE_C_COMPILER_FORCED TRUE)
+
+set(CMAKE_ASM_NASM_COMPILE_OBJECT
+        "<CMAKE_ASM_NASM_COMPILER> <FLAGS> -f win64 <INCLUDES> -o <OBJECT> <SOURCE>"
+)
+set(CMAKE_C_COMPILE_OBJECT
+        "<CMAKE_C_COMPILER> <FLAGS> -D EFI_PLATFORM=1 -target x86_64-pc-win32-coff <INCLUDES> -c -o <OBJECT> <SOURCE>"
+)
+
+set(CMAKE_C_FLAGS "-ffreestanding -fno-stack-protector -nostdlib -mno-red-zone -fshort-wchar")
+set(CMAKE_C_LINK_EXECUTABLE "lld -flavor link -subsystem:efi_application -entry:efi_main -nodefaultlib <OBJECTS> <LINK_LIBRARIES> /out:<TARGET>.EFI")
+
+include(FetchContent)
+
+if ("${efi-clang}" STREQUAL "")
+    FetchContent_Declare(
+            efi-clang-fetch
+            GIT_REPOSITORY https://github.com/yoppeh/efi.git
+    )
+
+    FetchContent_MakeAvailable(efi-clang-fetch)
+    message(STATUS "Fetched EFI-clang to ${efi-clang-fetch_SOURCE_DIR}")
+    set(efi-clang_SOURCE_DIR "${efi-clang-fetch_SOURCE_DIR}")
+else ()
+    set(efi-clang_SOURCE_DIR "${efi-clang}")
+endif ()
+message(STATUS "Using EFI-clang from ${efi-clang_SOURCE_DIR}")
+
+include_directories(${efi-clang_SOURCE_DIR})
