96c0daf9611bb8cc3a584ba0edac3cd23caa5bac
TheMonHub <mono@themonhub.net>
2026-02-19 00:14:09 +0700

    Refactor project structure and update formatting style; add logging functionality
    Signed-off-by: TheMonHub <mono@themonhub.net>

---
+++ src/file/file.c
@@ -0,0 +1,51 @@
+/*
+ * File: file.c
+ * Description: File-related functions.
+ *
+ * Copyright (C) 2025-2026 TheMonHub
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "file/file.h"
+
+#include <efi.h>
+#include <protocol/efi-lip.h>
+#include <protocol/efi-sfsp.h>
+#include <stddef.h>
+
+#include "main.h"
+
+EFI_GUID g_lip_guid = EFI_LOADED_IMAGE_PROTOCOL_GUID;
+EFI_GUID g_sfsp_guid = EFI_SIMPLE_FILE_SYSTEM_PROTOCOL_GUID;
+
+EFI_LOADED_IMAGE_PROTOCOL* get_loaded_image_protocol(EFI_HANDLE image_handle) {
+  EFI_LOADED_IMAGE_PROTOCOL* loaded_image = NULL;
+
+  g_system_table->BootServices->OpenProtocol(
+      image_handle, &g_lip_guid, (void**)&loaded_image, g_image_handle, NULL,
+      EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+
+  return loaded_image;
+}
+
+EFI_FILE_PROTOCOL* get_volume_handle(
+    const EFI_LOADED_IMAGE_PROTOCOL* loaded_image_protocol) {
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* sfsp;
+
+  EFI_FILE_PROTOCOL* volume_handle;
+
+  g_system_table->BootServices->OpenProtocol(
+      loaded_image_protocol->DeviceHandle, &g_sfsp_guid, (void**)&sfsp,
+      g_image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+  sfsp->OpenVolume(sfsp, &volume_handle);
+
+  return volume_handle;
+}
+
+UINT64 get_file_size(EFI_FILE_PROTOCOL* file) {
+  UINT64 size;
+  file->SetPosition(file, 0xFFFFFFFFFFFFFFFF);
+  file->GetPosition(file, &size);
+  file->SetPosition(file, 0);
+  return size;
+}
---
+++ src/output/logger.c
@@ -0,0 +1,9 @@
+/*
+ * File: logger.c
+ * Description: Logging helpers.
+ *
+ * Copyright (C) 2026 TheMonHub
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include "output/logger.h"
