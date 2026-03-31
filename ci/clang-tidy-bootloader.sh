b23728339df641d12d0ca17fbe5961ed978f7b7b
TheMonHub <mono@themonhub.net>
2026-03-31 22:57:48 +0700

    parallel ci
    Signed-off-by: TheMonHub <mono@themonhub.net>

---
+++ ci/clang-tidy-bootloader.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$0")"/.. || error_exit "Failed to change directory"
+echo "Running clang-tidy checks..."
+echo "================================"
+echo "Bootloader:"
+echo "================================"
+./bootloader/automated/check.sh
