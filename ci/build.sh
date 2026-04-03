eb7b04ffc76e690a156f3642b44bea004d0b9ebc
TheMonHub <mono@themonhub.net>
2026-04-03 20:25:28 +0700

    fix more oopsy
    Signed-off-by: TheMonHub <mono@themonhub.net>

--- ci/build.sh
+++ ci/build.sh
@@ -3,6 +3,6 @@ set -e
 
 cd "$(dirname "$0")"/.. || error_exit "Failed to change directory"
 echo "Running build tests..."
-mkdir -p build_bootloader
-cmake -S bootloader -B build_bootloader
-cmake --build build_bootloader
+mkdir -p build
+cmake -S . -B build
+cmake --build build
