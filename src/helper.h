3f11680a8f1314c9c9034146ed7ec928119b78be
TheMonHub <mono@themonhub.net>
2026-05-05 00:18:03 +0700

    Add parentheses to arguments in ERROR macro for safety.
    Signed-off-by: TheMonHub <mono@themonhub.net>

--- src/helper.h
+++ src/helper.h
@@ -2,7 +2,7 @@
 #define EFI_HELPER_H
 
 #define EFI_ERR(a) (0x8000000000000000 | (a))
-#define ERROR(a, b) (ErrorStatus) {.error_code = a, .status = b}
+#define ERROR(a, b) (ErrorStatus) {.error_code = (a), .status = (b)}
 #define SUCCESS (ErrorStatus) {.error_code = EFI_SUCCESS, .status = K_SUCCESS}
 
 #endif // EFI_HELPER_H
